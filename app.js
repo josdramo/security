09341ef626912c73c57a86119a78b27f0318e02a
josdramo <josdramo@espol.edu.ec>
2023-12-20 10:42:42 -0500

    jsonwebtoken

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const dotenv = require('dotenv');
 /* CARGA DE DATOS DE CONFIGURACION EN MEMORIA */
 dotenv.config();
 
+/* REFERENCIA AL MÓDULO */
+const swaggerUi = require('swagger-ui-express')
+
+/* REFERENCIA AL ARCHIVO GENERADO */
+const swaggerFile = require('./swagger_output.json')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -27,6 +33,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+/* CONFIGURACIÓN DE LA RUTA A LA DOCUMENTACIÓN */
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* Módulo jsonwebtoken */
+const jwt = require('jsonwebtoken');
+
 let crypto = require('crypto');
 
 /* Referencia a los modelos */
@@ -49,6 +52,34 @@ router.post('/register', async (req, res,next) => {
       res.status(400).send(error)
   }
 
-})
+});
+
+router.post('/generateToken', async (req, res,next) => {
+
+  // Parámetros en el cuerpo del requerimiento
+  let { name, password } = req.body;
+
+  try {
+
+  // Encripte la contraseña
+  let salt = process.env.SALT
+  let hash = crypto.createHmac('sha512', salt).update(password).digest("base64");
+  let passwordHash = salt + "$" + hash
+
+  /* Obtenga el usuario y su rol */
+  let user = await Users.findOne({ where: { [Op.and]: [ { name: name }, { password: passwordHash } ] } })
+  let relations = await UsersRoles.findOne({ where: { [Op.and]: [ { users_iduser: user.iduser } ] } });
+  let roles = await Roles.findOne({ where: { [Op.and]: [ { idrole: relations.roles_idrole } ] } });
+
+  /* Genera el token con los datos encriptados */
+  const accessToken = jwt.sign({ name: user.name, role: roles.name }, process.env.TOKEN_SECRET);
+
+  res.json({ accessToken });
+
+  } catch (error) {
+      res.status(400).send(error)
+  }
+
+});
 
 module.exports = router;
